b4e7508f3bdf2ba80766c4c7da43e42b69a99bb0
Nikita <smirnyh_ns@zrnc.ru>
2019-10-30 09:59:41 +0300

    Added simple classes for menu windows management

---
+++ Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    protected Stack<MenuWindow> _history;
+
+    void Start()
+    {
+        _history = new Stack<MenuWindow>();
+    }
+
+    public void AddToHistory(MenuWindow closedWondow)
+    {
+        _history.Push(closedWondow);
+    }
+
+    public void GoBack()
+    {
+        if (_history.Count > 0)
+        {
+            _history.Pop().Open();
+        }
+    }
+}
---
+++ MenuWindow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWindow : MonoBehaviour
+{
+    [Header("NextWindow")]
+    [SerializeField] protected MenuWindow _nextWindow;
+
+    [Header("Menu")]
+    [SerializeField] private Menu _menu;
+
+    protected virtual void Start()
+    {
+        if (_menu == null)
+        {
+            _menu = GetComponentInParent<Menu>();
+        }
+    }
+    
+    public virtual void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    protected virtual void Close()
+    {
+        _menu.AddToHistory(this);
+        gameObject.SetActive(false);
+    }
+
+    protected virtual void OpenNextWindow(MenuWindow nextWindow)
+    {
+        if(nextWindow == null)
+        {
+            Debug.LogAssertion("Window is not assigned", gameObject);
+            return;
+        }
+
+        Close();
+        nextWindow.Open();
+    }
+
+    public virtual void GoBack()
+    {
+        gameObject.SetActive(false);
+        _menu.GoBack();
+    }
+}
