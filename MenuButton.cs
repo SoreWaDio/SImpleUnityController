f0f750e0eb59db20d41d8154f1d094a448f8c0ac
SoreWaDio <57167766+SoreWaDio@users.noreply.github.com>
2019-10-30 09:02:25 +0300

    Init

---
+++ MenuButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public System.Action OnButtonClick;
+
+    protected virtual void Start()
+    {
+        var _button = GetComponent<Button>();
+        _button.onClick.AddListener(ClickAction);
+        
+    }
+
+    protected virtual void ClickAction()
+    {
+        OnButtonClick?.Invoke();
+        Debug.Log("click", gameObject);
+    }
+
+    protected virtual void HoverAction()
+    {
+        Debug.Log("hover", gameObject);
+    }
+
+    protected virtual void UnHoverAction()
+    {
+        Debug.Log("unhover", gameObject);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UnHoverAction();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        HoverAction();
+    }
+}
---
+++ MenuToggle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuToggle : MonoBehaviour
+{
+    protected GameObject _checkmark;
+
+    protected virtual void Start()
+    {
+        var _toggle = GetComponent<Toggle>();
+        _toggle.onValueChanged.AddListener(SetAction);
+
+        _checkmark = _toggle.graphic.gameObject;
+
+        //add to toggle group
+        var _group = GetComponentInParent<ToggleGroup>();
+        if (_group != null)
+        {
+            _toggle.group = _group;
+        }
+    }
+
+    protected virtual void SetAction(bool value)
+    {
+        Debug.Log("set" + value, gameObject);
+    }
+}
